dbc552005bafc2f8bd41473374c1e990b74a2f9a
Vyome Agarwal <vyome@ualberta.ca>
2022-01-28 14:35:57 -0700

    formatted better and restored the og runner.sh

--- server.py
+++ server.py
@@ -32,8 +32,6 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        print("hello")
 
         # check if self.data is empty
         if self.data == "":
@@ -42,29 +40,30 @@ class MyWebServer(socketserver.BaseRequestHandler):
         # check if the method is allowed: only GET
         if self.data.split()[0].decode() != "GET":
            
-            self.request.sendall(bytearray(self.get_error_response("405"),'utf-8'))
+            self.request.sendall(bytearray(self.get_response("405"),'utf-8'))
             return
 
-        print("HERE: ", self.data.split())
+        new_data = self.data
         
-        self.getGET(self.data)
-        
-        
-        
+        self.getGET(new_data)
+
         return
     
 
     def getGET(self, data):
+        """
+        Processes the GET command
+        Inputs:
+            data: the data regarding the request being made
+        Outputs:
+            None
+        """
 
         # get the direcoty www is in right now
         cwd = os.getcwd()
         cwd_www = os.path.join(cwd, "www")
 
-        
-
         file_path_from_request = data.split()[1].decode()
-        # print(data)
-        print("FILE PATH ", file_path_from_request)
 
         # attach the path to ...../www/
         if (file_path_from_request[0] == "/"):
@@ -72,38 +71,33 @@ class MyWebServer(socketserver.BaseRequestHandler):
         else:
             file_path = os.path.join(cwd_www, file_path_from_request)
         abs_file_path = os.path.realpath(file_path) # returns the absolute path: removes the ".././.." stuff
-        #print(abs_file_path)
 
-        # check if are in the correct directory
+        # check if are in the correct directory (https://www.geeksforgeeks.org/python-os-path-commonprefix-method/)
         if os.path.commonprefix([cwd_www, abs_file_path]) != cwd_www:
-            self.request.sendall(bytearray(self.get_error_response("404"),'utf-8'))
+            self.request.sendall(bytearray(self.get_response("404"),'utf-8'))
             return
         
         # check if abs_file_path is valid and if there is a trailing /
         if os.path.isdir(abs_file_path) and not file_path_from_request.endswith("/"):
-            print("HELLLLLLLLLL")
-            self.request.sendall(bytearray(self.get_error_response("301", abs_file_path),'utf-8'))
+            self.request.sendall(bytearray(self.get_response("301", path=abs_file_path),'utf-8'))
             return
 
+        # if abs_file_path is a directoy, open index.html in it (https://stackoverflow.com/a/15077441/11656468)
         if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
-            #print(abs_file_path)
             abs_file_path = os.path.join(abs_file_path, "index.html")
            
+        # if it neither a file not a directory, return 404
         elif not os.path.exists(abs_file_path):
-            # print(cwd_www)
-            # print(file_path_from_request)
-            print("YEEEEE")
-            print("PATH: ", abs_file_path)
-            self.request.sendall(bytearray(self.get_error_response("404", abs_file_path),'utf-8'))
+            self.request.sendall(bytearray(self.get_response("404", abs_file_path),'utf-8'))
             return
 
         file = open(abs_file_path, "r")
         file_data = file.read()
         
         if abs_file_path.endswith(".css"):
-            self.request.sendall(bytearray(self.get_error_response("200", content_type="css", body=file_data.encode()),'utf-8'))
+            self.request.sendall(bytearray(self.get_response("200", content_type="css", body=file_data.encode()),'utf-8'))
         else:
-            self.request.sendall(bytearray(self.get_error_response("200", content_type="html", body=file_data.encode()),'utf-8'))
+            self.request.sendall(bytearray(self.get_response("200", content_type="html", body=file_data.encode()),'utf-8'))
 
 
         file.close()
@@ -112,6 +106,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
 
     def get_status_code_description(self, status_code):
+        """
+        Returns the description of the Status code
+        Inputs:
+            status_code: the status code
+        Outputs:
+            The description of the status code
+        """
         status_code_descriptions = {
             "405": "Method Not Allowed",
             "301": "Moved Permanently",
@@ -121,31 +122,36 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
         return status_code_descriptions[status_code]
     
-    def get_error_response(self, status_code, path=None, content_type=None, body=None):
-        error_message = ""
+    def get_response(self, status_code, content_type=None, path=None, body=None):
+        """
+        Generates a response for each status_code
+        Inputs:
+            status_code: the status code
+            content_type: html or css
+            path: the path of the file
+            body: the body of the file to be rendered, if any
+        Output:
+            The generated response
+        """
         if status_code == "405":
             # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/405
-            error_message = f"<p>{self.get_status_code_description(status_code)}</p>"
 
-            error_response = "HTTP/1.1 405 Method Not Allowed\r\nAllow: GET\r\nContent-length:" + str(len(error_message)) + "\r\nContent-Type: text/html\r\n\r\n" + error_message
+            http_response = f"HTTP/1.1 {status_code} {self.get_status_code_description(status_code)}\r\nAllow: GET\r\n"
 
-            return error_response
+            return http_response
         
         elif status_code == "301":
             # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/301
-            error_message = f"<p>{self.get_status_code_description(status_code)}</p>"
 
-            error_response = f"HTTP/1.1 301 Moved Permanently\r\nLocation: {path}/\r\nContent-length:" + str(len(error_message)) + "\r\nContent-Type: text/html\r\n\r\n" + error_message
-            print("GGGGGGG")
-            return error_response
+            http_response = f"HTTP/1.1 {status_code} {self.get_status_code_description(status_code)}\r\n Location: {path}/"
+            return http_response
 
         elif status_code == "404":
             # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404
-            error_message = f"<p>{self.get_status_code_description(status_code)}</p>"
 
-            error_response = f"HTTP/1.1 404 Not Found\r\nContent-length:" + str(len(error_message)) + "\r\nContent-Type: text/html\r\n\r\n" + error_message
+            http_response = f"HTTP/1.1 {status_code} {self.get_status_code_description(status_code)}"
 
-            return error_response
+            return http_response
         
         
         elif status_code == "200":
@@ -155,13 +161,9 @@ class MyWebServer(socketserver.BaseRequestHandler):
             if (content_type == None):
                 use_body = f"<p>{self.get_status_code_description(status_code)}</p>"
 
-            error_response = f"HTTP/1.1 200 OK\r\nContent-length:" + str(len(body)) + f"\r\nContent-Type: text/{content_type}\r\n\r\n" + use_body.decode()
+            http_response = f"HTTP/1.1 {status_code} {self.get_status_code_description(status_code)}\r\nContent-length:" + str(len(body)) + f"\r\nContent-Type: text/{content_type}\r\n\r\n" + use_body.decode()
 
-            return error_response
-        
-
-
-        #self.request.sendall(bytearray("OK",'utf-8'))
+            return http_response
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
